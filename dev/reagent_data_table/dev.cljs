ef25755096adbbad2ce90ce2e8e8709910ea3f6e
Matthew Gilliard <matthew.gilliard@gmail.com>
2016-12-06 09:35:25 +0000

    Pass the atom to the component as an argument
    Fixes #1 - the demo app was not deref-ing the atom within the component
    so reagent wasn't updating the table when the atom content changed.

--- dev/reagent_data_table/dev.cljs
+++ dev/reagent_data_table/dev.cljs
@@ -13,35 +13,46 @@
    {:id 4 :name "David"   :age 41}
    {:id 5 :name "Everlasting Ermine"}])
 
+(def data2
+  [{:id 6  :name "Freddy Five Fingers"  :age 23}
+   {:id 7  :name "Grateful Gertie"      :age 28}
+   {:id 8  :name "Harmful Humphrey"     :age 32}
+   {:id 9  :name "Ignacius In An Igloo" :age 41}
+   {:id 10 :name "Jovial Joliet"        :age 54}])
+
 (def app-state (r/atom {:table-data data1}))
 
-(r/render-component
+(defn page [app]
+  [:div
 
- [:div
+   [:h2 "Data"]
+   [:input {:type "Button" :value "Dataset #1" :on-click #(swap! app-state assoc :table-data data1)}]
+   [:input {:type "Button" :value "Dataset #2" :on-click #(swap! app-state assoc :table-data data2)}]
 
-  [:h2 "A very plain table"]
-  [:div.table-container
-   [rdt/data-table {:headers [[:id "ID"] [:name "Name"] [:age "Age"]]
-                    :rows    (:table-data @app-state)}]]
+   [:h2 "A very plain table"]
+   [:div.table-container
+    [rdt/data-table {:headers [[:id "ID"] [:name "Name"] [:age "Age"]]
+                     :rows    (:table-data @app)}]]
 
 
-  [:h2 "A snazzy table"]
-  [:div.table-container
-   [rdt/data-table {:table-id "table-1"
+   [:h2 "A snazzy table"]
+   [:div.table-container
+    [rdt/data-table {:table-id "table-1"
 
-                    :headers [[:id "ID"] [:name "Name"] [:age "Age"]]
-                    :rows    (:table-data @app-state)
+                     :headers [[:id "ID"] [:name "Name"] [:age "Age"]]
+                     :rows    (:table-data @app-state)
 
-                    :no-data-label [:span.info "~~unknowable~~"]
+                     :no-data-label [:span.info "~~unknowable~~"]
 
-                    :filterable-columns [:age :name]
-                    :filter-label "Search by age or name:"
-                    :filter-string "e"
+                     :filterable-columns [:age :name]
+                     :filter-label "Search by age or name:"
+                     :filter-string "e"
 
-                    :sortable-columns [:id :name :age]
-                    :sort-columns [[:age true]]
-                    :sort-image-base "img/"
+                     :sortable-columns [:id :name :age]
+                     :sort-columns [[:age true]]
+                     :sort-image-base "img/"
 
-                    :table-state-change-fn #(.log js/console %)}]]]
+                     :table-state-change-fn #(.log js/console %)}]]]  )
 
+(r/render-component [page app-state]
  (.getElementById js/document "dt-app"))
